24af35ca3a98185c376b890c7805beb16ac478a4
Jason Hsu <jhsu@crunchbang.(none)>
2014-11-02 18:52:22 -0600

    test_rvm.sh now works

--- shared/test_rvm.sh
+++ shared/test_rvm.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-git clone https://github.com/railstutorial/sample_app_rails_4.git
-cd sample_app_rails_4
-rvm install ruby-2.0.0-p594
-cp config/database.yml.example config/database.yml
-bundle install --without production
-rake db:migrate
-rake db:test:prepare
-rspec spec/
-rails server
+gem install copy_rails_tutorial
+copy_rails_tutorial_all 'test_app' 'TestApplication'
+cd test_app && sh setup.sh
