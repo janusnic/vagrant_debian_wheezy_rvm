2d240dfe610282fbc2eddafc8f6a6b2bd4ad5b2b
Jason Hsu <jhsu@crunchbang.(none)>
2014-12-11 01:25:38 -0600

    school app from Raspberry Pi page, stick with default version of Rails but skip the installation of Spring so that the script does not freeze up

--- shared/test_rvm.sh
+++ shared/test_rvm.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# Just making sure Ruby, Rails, and Node are working as expected.
 rvm -v
 ruby -v
 rails -v
 node -v
 
-cd /home/vagrant/shared && rails new school
+# Due to a bug, installing Spring causes Ruby on Rails to freeze during 
+# the "bundle install" process.  Therefore, we are skipping this step so 
+# that this script can continue.  Yes, this is the same test app as the 
+# one at http://elinux.org/RPi_Ruby_on_Rails .
+
+cd /home/vagrant/shared && rails new school --skip-spring
 cd /home/vagrant/shared/school && rails g scaffold Pupil name:string form:string
 cd /home/vagrant/shared/school && rake db:migrate
+echo "Open your web browser to the following URL:"
+echo "http://localhost:3000/pupils"
 cd /home/vagrant/shared/school && rails s
