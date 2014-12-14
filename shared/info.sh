e60ca76f85deb68af4f2ae9adde131c5e8515c07
Jason Hsu <jhsu@crunchbang.(none)>
2014-12-14 10:58:33 -0600

    Added versions of Redis, Postgres, and SQL to shared/info.sh; added this info to changelog

--- shared/info.sh
+++ shared/info.sh
@@ -2,16 +2,46 @@
 
 . ~/.nvm/nvm.sh # NVM command does not work in this script without this.
 
+echo
+
 TIME_STAMP="$(cat /home/vagrant/timestamp.txt)"
 echo "Timestamp: ${TIME_STAMP}"
+
+echo
 rvm -v
+
+echo
 ruby -v
+
+echo
 rails -v
-python --version
-VERSION_PUPPET="$(puppet master --version)"
-echo "Version of Puppet: ${VERSION_PUPPET}"
-chef-solo -v
+
+echo
 VERSION_NVM="$(nvm --version)"
 echo "NVM: ${VERSION_NVM}"
+
+echo
 VERSION_NODE="$(node -v)"
 echo "node.js: ${VERSION_NODE}"
+
+echo
+python --version
+
+echo
+VERSION_PUPPET="$(puppet master --version)"
+echo "Version of Puppet: ${VERSION_PUPPET}"
+
+echo
+chef-solo -v
+
+echo
+redis-server -v
+
+echo
+echo "Version of SQLite:"
+sqlite3 -version
+
+echo
+psql --version
+
+echo
