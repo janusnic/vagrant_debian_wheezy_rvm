28bb15e44ed6026d94e4c65691388fbdf0477591
Jason Hsu <jhsu@crunchbang.Home>
2015-01-01 15:53:53 -0600

    update_ror.sh updates Rails in latest Ruby gemset

--- shared/update_ror.sh
+++ shared/update_ror.sh
@@ -4,8 +4,9 @@ if [ "$USER" = "vagrant" ]
 then
   rvm install ruby --latest # Install the latest stable version of Ruby
   rvm use ruby --latest
-  rvm install rails --latest
+  gem install rails
   rvm list rubies
+  rails -v
 else
   echo "You MUST be user 'vagrant' to execute this script."
 fi
