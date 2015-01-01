289a3b3da3b2827d35b4a99dbe9301e286faf24c
Jason Hsu <jhsu@crunchbang.Home>
2015-01-01 13:43:39 -0600

    Updated changelog to include list of rubies

--- shared/update_linux.sh
+++ shared/update_linux.sh
@@ -5,6 +5,6 @@ then
   sudo apt-get update
   sudo apt-get upgrade -y
 else
-  echo "You MUST be user 'vagrant' to execute this script"
+  echo "You MUST be user 'vagrant' to execute this script."
 fi
 
