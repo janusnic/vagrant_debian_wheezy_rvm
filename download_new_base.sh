3285a9d88498c45d43805703a385d3f56d269d29
Jason Hsu <jhsu@crunchbang.Home>
2015-01-02 19:42:01 -0600

    Updated Vagrantfile for version 2015_01_02; added download_new_base.sh

---
+++ download_new_base.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Proper header for a Bash script.
+
+# NOTE: This script is for use by the Packer setup at 
+# https://github.com/jhsu802701/packer-debian-wheezy-rvm .
+
+# Check for regular user login
+if [ ! $( id -u ) -ne 0 ]; then
+  echo "You must be a regular user to run this script."
+  exit 2
+fi 
+
+echo "------------"
+echo "vagrant halt"
+vagrant halt
+
+echo "---------------"
+echo "vagrant destroy"
+vagrant destroy
+
+echo "------------------------------------"
+echo "vagrant box remove debian-wheezy-rvm"
+vagrant box remove debian-wheezy-rvm;
+
+sh login.sh
