14250266e8ce016d07c6b5b1664437ffeb4d9517
Jason Hsu <jhsu@crunchbang.Home>
2015-01-28 12:21:13 -0600

    shared/update_rvm.sh works

--- shared/update_rvm.sh
+++ shared/update_rvm.sh
@@ -2,12 +2,12 @@
 
 if [ "$USER" = "vagrant" ]
 then
-  echo "RVM update deactivated"
-  # rvm get head
-  # rvm reload
-  # rvm get stable
-  # rvm reload
-  # rvm -v
+  # echo "RVM update deactivated"
+  rvm get head
+  rvm reload
+  rvm get stable
+  rvm reload
+  sh /home/vagrant/shared/info.sh
 else
   echo "You MUST be user 'vagrant' to execute this script."
 fi
