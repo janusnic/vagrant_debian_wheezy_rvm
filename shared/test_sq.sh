58474a2c069f57e7f8522898c6888c16ca20dcb6
Jason Hsu <jhsu@crunchbang.Home>
2015-02-02 23:03:55 -0600

    Added test_pg.sh and README-postgres.txt

--- shared/test_sq.sh
+++ shared/test_sq.sh
@@ -6,12 +6,8 @@
 # Just making sure Ruby, Rails, and Node are working as expected.
 sh /home/vagrant/shared/info.sh
 
-# Due to a bug, installing Spring causes Ruby on Rails to freeze during 
-# the "bundle install" process.  Therefore, we are skipping this step so 
-# that this script can continue.  Yes, this is the same test app as the 
-# one at http://elinux.org/RPi_Ruby_on_Rails .
-
-cd /home/vagrant/shared && rails new school
+# This is the same test app as the one at http://elinux.org/RPi_Ruby_on_Rails .
+cd /home/vagrant/shared && rails new school --skip-spring
 cd /home/vagrant/shared/school && rails g scaffold Pupil name:string form:string
 cd /home/vagrant/shared/school && rake db:migrate
 echo "**********************"
